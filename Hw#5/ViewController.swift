8e3f20235fe5c9fea73120e48a315a092b73ca3b
Zahar Litvinchuk <97597336+raynelz@users.noreply.github.com>
2023-11-05 17:08:25 +0300

    Initial Commit

---
+++ Hw#5/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Hw#5
+//
+//  Created by Захар Литвинчук on 05.11.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
